37e78ef89f731424b4c332a6ad5cc5b1bc5181a4
JonathanTubac <javiertubac1290.e@gmail.com>
2026-04-07 03:07:13 -0600

    feat(frontend): Added Pill component

--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -2,31 +2,39 @@
   <main>
     <Navbar />
     <section id="header">
-      <AnimatedText
-      style="margin-top: 125px"
-        effects="split shiny"
-        text="Take control of your bussiness"
-        tag="h1"
-        class="hero-title"
-        color="var(--Primary)"
-        shine-color="var(--Text)"
-        :speed="5"
-        :delay="2.4"
-        :spread="135"
-        direction="left"
-        :yoyo="false"
-        :pause-on-hover="false"
-        :disabled="false"
-        :split-delay="50"
-        :duration="1.25"
-        ease="power3.out"
-        :from="{ opacity: 0, y: 40 }"
-        :to="{ opacity: 1, y: 0 }"
-        text-align="center"
-      />
-      <p style="color: white">
-        Organiza tus tareas de manera eficiente y mantente productivo.
-      </p>
+      <div class="header-container">
+        <Pill 
+          label="The future of Business"
+          btnColor="var(--PillBack)"
+          circleColor="var(--Primary)"
+          textColor="var(--Primary)"
+        />
+        <AnimatedText
+          style="margin-top: 0"
+          effects="split shiny"
+          text="Take control of your bussiness"
+          tag="h1"
+          class="hero-title"
+          color="var(--Primary)"
+          shine-color="var(--Text)"
+          :speed="5"
+          :delay="2.4"
+          :spread="135"
+          direction="left"
+          :yoyo="false"
+          :pause-on-hover="false"
+          :disabled="false"
+          :split-delay="50"
+          :duration="1.25"
+          ease="power3.out"
+          :from="{ opacity: 0, y: 40 }"
+          :to="{ opacity: 1, y: 0 }"
+        />
+        <p>
+          Manage projects, finances, marketing and communication from one
+          centralized platform. Designed for the ambitious.
+        </p>
+      </div>
     </section>
 
     <section id="problem"></section>
@@ -35,13 +43,9 @@
   </main>
 </template>
 
-<script>
+<script setup>
+import "./LandingPage.css";
 import Navbar from "../components/Navbar.vue";
 import AnimatedText from "../components/UI/TextEffects/AnimatedText.vue";
-export default {
-  components: {
-    Navbar,
-    AnimatedText,
-  },
-};
+import Pill from "../components/UI/Pill/Pill.vue"
 </script>
