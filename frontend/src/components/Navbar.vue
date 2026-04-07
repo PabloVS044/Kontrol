a3eacd10bf94916da70c3c23b94df1f3ee7956bb
JonathanTubac <javiertubac1290.e@gmail.com>
2026-04-06 19:45:50 -0600

    feat(frontend): Added the landing page view

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav>
+        <img src="" alt="">
+        <button>Features</button>
+        <button>How it works?</button>
+        <button>Pricing</button>
+        <button>About</button>
+        <button>Contact</button>
+    </nav>
+</template>
---
+++ frontend/src/components/UI/Button/Button.vue
@@ -0,0 +1,33 @@
+<template>
+    <button class="btn" :type="type" :disabled="disabled" @click="handleClick">
+        {{ label }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        label: {
+            type: String,
+            default: 'Button'
+        },
+        type: {
+            type: String,
+            default: 'button',
+            validator: value => ['button', 'submit', 'reset'].includes(value)
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        handleClick() {
+            this.$emit('click');
+        }
+    }
+}
+</script>
+
+<style src="./Button.css" scoped></style>
