a99a83060a689624298d5028d77984d74f54b611
JonathanTubac <javiertubac1290.e@gmail.com>
2026-04-06 19:45:50 -0600

    feat(frontend): Added the landing page view

---
+++ frontend/src/views/LandingPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <Navbar />
+    <section id="header">
+      <h1>Bienvenido a la aplicación de gestión de tareas</h1>
+      <p>Organiza tus tareas de manera eficiente y mantente productivo.</p>
+    </section>
+
+    <section id="problem">
+
+    </section>
+
+    <section id="solution">
+
+    </section>
+  </main>
+</template>
+
+<script>
+    import Navbar from '../components/Navbar.vue';
+</script>
