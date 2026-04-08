3821517d520095c5767500576d688fe80cbf6ea8
SarAvi21805 <alejandraviles2005@gmail.com>
2026-04-08 12:16:31 -0600

    feat: add dashboard route to router configuration

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import InventoryPage from '../views/InventoryPage.vue'
 import ProjectsView from '../views/ProjectsView.vue'
+import DashboardView from '../views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/projects',
       name: 'projects',
       component: ProjectsView
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView
     }
   ]
 })
