ccc6c9d09b138ac60516b5092c7d891cec8abe6f
SarAvi21805 <alejandraviles2005@gmail.com>
2026-04-07 20:19:45 -0600

    feat: Se añadió la ruta en index.js para validar el código realizado para ProjectsView.vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import InventoryPage from '../views/InventoryPage.vue'
+import ProjectsView from '../views/ProjectsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/inventory',
       name: 'inventory',
       component: InventoryPage
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: ProjectsView
     }
   ]
 })
