cde501a1b62aa8054c798363256e3ad1f7e68fe1
SarAvi21805 <alejandraviles2005@gmail.com>
2026-08-09 21:09:25 -0600

    style(anchor): enhance style properties for better default values and consistency

--- frontend/src/components/UI/Button/Anchor.vue
+++ frontend/src/components/UI/Button/Anchor.vue
@@ -3,25 +3,28 @@
     class="anchor"
     :href="link"
     :style="{
-      '--background': backColor,
-      '--hover': hoverColor,
-      '--color': textColor,
+      '--background': backColor || 'var(-Primary)',
+      '--hover': hoverColor || 'var(--Primary2)',
+      '--color': textColor || 'var(--BtnText)',
     }"
   >
-    {{ label }}
-    <component v-if="icon && typeof icon !== 'string'" :is="icon" :size="18" />
+    <!-- Label del botón -->
+    <span v-if="label">{{ label }}</span>
+
+    <component v-if="icon && typeof icon !== 'string'" :is="icon" :size="16" />
     <img v-else-if="icon" :src="icon" :alt="label" />
   </a>
 </template>
+
 <script setup>
 import "./Anchor.css";
 
 const props = defineProps({
-  label: { type: String, default: "LinkButton" },
-  link: { type: String, default: "" },
+  label: { type: String, default: "" },
+  link: { type: String, default: "#" },
   backColor: { type: String, default: "" },
-  hoverColor: { type: String, default: "var(--Primary)" },
-  textColor: { type: String, default: "var(--Text)" },
+  hoverColor: { type: String, default: "" },
+  textColor: { type: String, default: "" },
   icon: { type: [String, Object, Function], default: "" },
 });
 </script>
